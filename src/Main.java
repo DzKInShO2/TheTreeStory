afcd0c06af037134334cafe940dcb4f89867ee50
DzKInShO2 <dzkinsho2@protonmail.com>
2024-12-18 08:05:53 +0800

    Menggambar visualisasi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        Parser parser = Parser.loadFile("res/scene.str");
 
+        Scene scene = parser.nextScene();
+
+        System.out.printf("+----------------+\n");
+        for (int i = 0; i < scene.graphics.length; ++i) {
+            System.out.printf("|");
+            System.out.printf(scene.graphics[i]);
+            System.out.printf("|\n");
+        }
+        System.out.printf("+----------------+\n");
     }
 }
