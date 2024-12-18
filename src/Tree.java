1ee53203af28b6babed44e1a3902d0c2bbb3ce57
F1D02310048 <fadilarahmania04@gmail.com>
2024-12-18 17:57:34 +0800

    Menambahkan fungsi insert dan level order

--- src/Tree.java
+++ src/Tree.java
@@ -2,6 +2,7 @@
  * Kelas <Node> ini digunakan sebagai node
  * yang akan digunakan oleh kelas Tree
  */
+
 class Node {
     Node next, prev;
     Scene scene;
@@ -56,6 +57,96 @@ public class Tree {
         }
         return 1 + countNodes(node.next) + countNodes(node.prev);
     }
+
+     public void insertOrderTraversal(Scene scene) {
+        Node newNode = new Node(scene);
+        if (root == null) {
+            root = newNode;
+        } else {
+            insertOrderTraversal(root, newNode);
+        }
+    }
+
+    private void insertOrderTraversal(Node current, Node newNode) {
+        if (newNode.scene.idScene < current.scene.idScene) {
+            if (current.next == null) {
+                current.next = newNode;
+            } else {
+                insertOrderTraversal(current.next, newNode);
+            }
+        } else {
+            if (current.prev == null) {
+                current.prev = newNode;
+            } else {
+                insertOrderTraversal(current.prev, newNode);
+            }
+        }
+    }
+
+    public Scene[] traverseLevelOrder() {
+        int size = countNodes(root);
+        Scene[] scenes = new Scene[size];
+        if (root == null) {
+            return scenes;
+        }
+
+        Queue queue = new Queue(size);
+        queue.enqueue(root);
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.dequeue();
+            scenes[index++] = node.scene;
+
+            if (node.next != null) {
+                queue.enqueue(node.next);
+            }
+            if (node.prev != null) {
+                queue.enqueue(node.prev);
+            }
+        }
+        return scenes;
+    }
+
+    private static class Queue {
+        private Node[] elements;
+        private int front;
+        private int rear;
+        private int size;
+        private int capacity;
+
+        public Queue(int capacity) {
+            this.capacity = capacity;
+            elements = new Node[capacity];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+
+        public void enqueue(Node node) {
+            if (size == capacity) {
+                throw new IllegalStateException("Queue is full");
+            }
+            rear = (rear + 1) % capacity;
+            elements[rear] = node;
+            size++;
+        }
+
+        public Node dequeue() {
+            if (isEmpty()) {
+                throw new IllegalStateException("Queue is empty");
+            }
+            Node node = elements[front];
+            front = (front + 1) % capacity;
+            size--;
+            return node;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+    }
+
 }
 
 
