64c02031be095f910c15858c8a26749cede2a8ea
DevitaAmalia <devitaamalia30@gmail.com>
2024-12-21 10:39:39 +0800

    update stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,34 +1,39 @@
 public class Stack {
-    public Scene[] stack; // Array untuk menyimpan Scene
-    public int top;       
+    public Scene top; 
+    public Stack next; 
 
-    
-    public SceneStack(int size) {
-        this.stack = new Scene[size]; // Ukuran array ditentukan saat pembuatan stack
-        this.top = -1; 
+    public Stack() {
+        this.top = null;
+        this.next = null;
     }
 
     // Method untuk menambahkan Scene ke stack
     public void push(Scene scene) {
-        if (top == stack.length - 1) {
-            System.out.println("Stack is full! Cannot push.");
-        } else {
-            stack[++top] = scene;
-        }
+        Stack newNode = new Stack(); // Membuat node baru
+        newNode.top = scene;         // Menyimpan Scene di node baru
+        newNode.next = this.next;    // Menghubungkan node baru ke stack lama
+        this.next = newNode;         // Node baru menjadi elemen pertama
     }
 
     // Method untuk menghapus (pop) Scene dari stack
-    public Scene pull() {
-        if (top == -1) { // Mengecek apakah stack kosong
-            System.out.println("Stack is empty! Cannot pull.");
+    public Scene pop() {
+        if (this.next == null) { // Mengecek apakah stack kosong
+            System.out.println("Scene is empty! Cannot pull.");
             return null;
         } else {
-            return stack[top--];
+            Scene pulledScene = this.next.top; // Simpan elemen teratas
+            this.next = this.next.next;       // Hapus elemen teratas
+            return pulledScene;
         }
     }
 
-    // Method untuk mendapatkan jumlah elemen dalam stack
-    public int size() {
-        return top + 1;
+    // Method untuk melihat elemen teratas (top) tanpa menghapusnya
+    public Scene peek() {
+        if (this.next == null) { // Mengecek apakah stack kosong
+            System.out.println("Scene is empty! Cannot peek.");
+            return null;
+        } else {
+            return this.next.top;
+        }
     }
 }
